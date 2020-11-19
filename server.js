323554af344af0ef5647dbb1979b607b38d0c397
tang0614 <tang0614@bu.edu>
2020-11-19 10:37:27 -0500

    hw6

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+require('./controllers/quizzes.controller.server')(app)
+require('./controllers/question.controller.server')(app)
+
+app.listen(3000)
+
